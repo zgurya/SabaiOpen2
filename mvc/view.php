ebc80745e1b1b7c151d2285bf1d234b42b9f3d84
wordpress <wordpress@miritec-xvii>
2017-01-26 20:20:43 +0200

    Add default template

--- mvc/view.php
+++ mvc/view.php
@@ -1,7 +1,11 @@
 <?php
 class View {
-	public function template($file) {
-		$this->tmplf=$file;
+	public function template($file=null) {
+		if($file){
+			$this->tmplf=$file;
+		}else{
+			$this->tmplf='administration.status';
+		}
 		require_once 'views/template.php';
 	}
 }
