f4179f1d8dc34e536ec20bf6e8a85e8ff095da61
wordpress <wordpress@miritec-xvii>
2017-01-26 19:49:06 +0200

    Add system status block

---
+++ views/administration.status.php
@@ -0,0 +1,79 @@
+<div id="system">
+	<div class="row">
+		<div class="col-12">
+			<h2>System</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<table class="controlTable">
+				<tbody class="smallText">
+					<tr>
+						<td class="statusCellName">
+							<b>Name</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_name">SabaiOpen</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>Model</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_model"> Intel(R) Atom(TM) CPU  E3827  @ 1.74GHz</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>Version Build</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_version">SomChai Router 12-28-16_11-56</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>Time</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_time">Thu Jan 26 12:43:35 EST 2017</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>Uptime</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_uptime">13 days</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>CPU Load</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_cpuload"></div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>Free Mem</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_mem">3870204 kB</div>
+						</td>
+					</tr>
+					<tr>
+						<td class="statusCellName">
+							<b>Sys Gateway</b>
+						</td>
+						<td>
+							<div class="statusCellContent" id="sys_gateway">192.168.88.1</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
