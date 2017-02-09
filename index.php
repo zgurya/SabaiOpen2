a2d13c62fb39fe46031a3d17b98867c807b3abe8
wordpress <wordpress@miritec-xvii>
2017-02-09 12:22:26 +0200

    Add timezone

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 define('SITE_URL', (!empty($_SERVER['HTTPS']))? 'https://'.$_SERVER['HTTP_HOST']:'http://'.$_SERVER['HTTP_HOST']);
 define('ROOT', __DIR__);
 define('SESSION_TIMEOUT',1800);
+date_default_timezone_set(exec('date +%Z'));
 
 require_once 'mvc/router.php';
 require_once 'mvc/controller.php';
