0f379f8ed855cef8e1c95ae625f3588d3c0a8999
wordpress <wordpress@miritec-xvii>
2017-02-13 13:46:09 +0200

    Change URLs

--- www/views/template.php
+++ www/views/template.php
@@ -25,53 +25,53 @@
 					<li>
 						<a href="#" class="parent-menu">Network</a>
 						<ul id="submenu_network" class="sub-menu">
-							<li><a id="menu_network-wan" href="<?php echo SITE_URL;?>/network/wan/">WAN</a></li>
-					 		<li><a id="menu_network-lan" href="<?php echo SITE_URL;?>/network/lan/">LAN</a></li>
-					 		<li><a id="menu_network-time" href="<?php echo SITE_URL;?>/network/time/">Time</a></li>
+							<li><a id="menu_network-wan" href="<?php echo SITE_URL;?>?url=/network/wan/">WAN</a></li>
+					 		<li><a id="menu_network-lan" href="<?php echo SITE_URL;?>?url=/network/lan/">LAN</a></li>
+					 		<li><a id="menu_network-time" href="<?php echo SITE_URL;?>?url=/network/time/">Time</a></li>
 						</ul>
 					</li>
 					<li>
 						<a href="#" class="parent-menu">Wireless</a>
 						<ul id="submenu_wireless" class="sub-menu">
-							<li><a id="menu_wireless-radio" href="<?php echo SITE_URL;?>/wireless/radio/">Radio</a></li>
+							<li><a id="menu_wireless-radio" href="<?php echo SITE_URL;?>?url=/wireless/radio/">Radio</a></li>
 						</ul>
 					</li>
 					<li>
 						<a href="#" class="parent-menu">VPN</a>
 						<ul id="submenu_vpn" class="sub-menu">
-							<li><a id="menu_vpn-openvpnserver" href="<?php echo SITE_URL;?>/vpn/openvpnserver/">Server</a></li>
-							<li><a id="menu_vpn-openvpnclient" href="<?php echo SITE_URL;?>/vpn/openvpnclient/">OpenVPN Client</a></li>
-							<li><a id="menu_vpn-pptpclient" href="<?php echo SITE_URL;?>/vpn/pptpclient/">PPTP Client</a></li>
-							<li><a id="menu_vpn-tor" href="<?php echo SITE_URL;?>/vpn/tor/">Tor</a></li>
-							<li><a id="menu_vpn-gateways" href="<?php echo SITE_URL;?>/vpn/gateways/">Gateways</a></li>
+							<li><a id="menu_vpn-openvpnserver" href="<?php echo SITE_URL;?>?url=/vpn/openvpnserver/">Server</a></li>
+							<li><a id="menu_vpn-openvpnclient" href="<?php echo SITE_URL;?>?url=/vpn/openvpnclient/">OpenVPN Client</a></li>
+							<li><a id="menu_vpn-pptpclient" href="<?php echo SITE_URL;?>?url=/vpn/pptpclient/">PPTP Client</a></li>
+							<li><a id="menu_vpn-tor" href="<?php echo SITE_URL;?>?url=/vpn/tor/">Tor</a></li>
+							<li><a id="menu_vpn-gateways" href="<?php echo SITE_URL;?>?url=/vpn/gateways/">Gateways</a></li>
 						</ul>
 					</li>
 					<li>
 						<a href="#" class="parent-menu">Diagnostics</a>
 						<ul id="submenu_diagnostics" class="sub-menu">
-							<li><a id="menu_diagnostics-ping" href="<?php echo SITE_URL;?>/diagnostics/ping/">Ping</a></li>
-							<li><a id="menu_diagnostics-trace" href="<?php echo SITE_URL;?>/diagnostics/trace/">Trace</a></li>
-							<li><a id="menu_diagnostics-nslookup" href="<?php echo SITE_URL;?>/diagnostics/nslookup/">NS Lookup</a></li>
-							<li><a id="menu_diagnostics-route" href="<?php echo SITE_URL;?>/diagnostics/route/">Route</a></li>
-							<li><a id="menu_diagnostics-logs" href="<?php echo SITE_URL;?>/diagnostics/logs/">Logs</a></li>
-							<li><a id="menu_diagnostics-console" href="<?php echo SITE_URL;?>/diagnostics/console/">Console</a></li>
+							<li><a id="menu_diagnostics-ping" href="<?php echo SITE_URL;?>?url=/diagnostics/ping/">Ping</a></li>
+							<li><a id="menu_diagnostics-trace" href="<?php echo SITE_URL;?>?url=/diagnostics/trace/">Trace</a></li>
+							<li><a id="menu_diagnostics-nslookup" href="<?php echo SITE_URL;?>?url=/diagnostics/nslookup/">NS Lookup</a></li>
+							<li><a id="menu_diagnostics-route" href="<?php echo SITE_URL;?>?url=/diagnostics/route/">Route</a></li>
+							<li><a id="menu_diagnostics-logs" href="<?php echo SITE_URL;?>?url=/diagnostics/logs/">Logs</a></li>
+							<li><a id="menu_diagnostics-console" href="<?php echo SITE_URL;?>?url=/diagnostics/console/">Console</a></li>
 						</ul>
 					</li>
 					<li>
 						<a href="#" class="parent-menu">Security</a>
 						<ul id="submenu_security" class="sub-menu">
-							<li><a id="menu_security-firewall" href="<?php echo SITE_URL;?>/security/firewall/">Firewall</a></li>
+							<li><a id="menu_security-firewall" href="<?php echo SITE_URL;?>?url=/security/firewall/">Firewall</a></li>
 							<li><a id="menu_security-portforwarding" href="<?php echo SITE_URL;?>/security/portforwarding/">Port Forwarding</a></li>
-							<li><a id="menu_security-dmz" href="<?php echo SITE_URL;?>/security/dmz/">DMZ</a></li>
-							<li><a id="menu_security-upnp" href="<?php echo SITE_URL;?>/security/upnp/">UPnP</a></li>
+							<li><a id="menu_security-dmz" href="<?php echo SITE_URL;?>?url=/security/dmz/">DMZ</a></li>
+							<li><a id="menu_security-upnp" href="<?php echo SITE_URL;?>?url=/security/upnp/">UPnP</a></li>
 						</ul>
 					</li>
 					<li>
 						<a href="#" class="parent-menu">Administration</a>
 						<ul id="submenu_administration" class="sub-menu">
-							<li><a id="menu_administration-settings" href="<?php echo SITE_URL;?>/administration/settings/">Settings</a></li>
-							<li><a id="menu_administration-upgrade" href="<?php echo SITE_URL;?>/administration/upgrade/">Upgrade</a></li>
-							<li><a id="menu_administration-status" href="<?php echo SITE_URL;?>/administration/status/">Status</a></li>
+							<li><a id="menu_administration-settings" href="<?php echo SITE_URL;?>?url=/administration/settings/">Settings</a></li>
+							<li><a id="menu_administration-upgrade" href="<?php echo SITE_URL;?>?url=/administration/upgrade/">Upgrade</a></li>
+							<li><a id="menu_administration-status" href="<?php echo SITE_URL;?>?url=/administration/status/">Status</a></li>
 						</ul>
 					</li>
 					<li>
